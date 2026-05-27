4171dbb6e0f3c223c948eab53e0363994baef601
Korejay1111 <aderogbaolajidejoseph@gmail.com>
2026-05-27 10:36:57 +0100

    commit

--- backend/app.py
+++ backend/app.py
@@ -108,7 +108,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://verifyit-3.onrender.com",
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
