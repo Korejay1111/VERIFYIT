92cd30baf06417d8815546886490bad69e4d682b
Korejay1111 <aderogbaolajidejoseph@gmail.com>
2026-05-28 14:20:08 +0100

    commit

--- backend/app.py
+++ backend/app.py
@@ -110,7 +110,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
          "https://verifyit-3.onrender.com",
-        "https://https://verifyit-ten.vercel.app",
+        "https://verifyit-ten.vercel.app",
         "https://verifyit-ten.vercel.app/home.html",
         "http://localhost:8000",
         "http://127.0.0.1:5500",
